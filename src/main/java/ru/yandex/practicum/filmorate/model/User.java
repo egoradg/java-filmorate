efa8d842674ebe3a0502fc558c9216672a0696cf
egor <egorr12122006@gmail.com>
2026-08-28 14:13:21 +0300

    добавить классы Film и User

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+public class User {
+    private Long id;
+    private String email;
+    private String login;
+    private String name;
+    private Instant birthday;
+}
