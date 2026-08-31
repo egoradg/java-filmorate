008ddb2cff3916ff2b10c074fd0ae5a2292f5f1a
egor <egorr12122006@gmail.com>
2026-08-31 19:47:26 +0300

    добавить билдеры для Film и User

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.*;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -9,16 +10,21 @@ import java.time.LocalDate;
  * Film.
  */
 @Data
+@Builder
 public class Film {
     private Long id;
+
     @NotNull
     @NotBlank
     private String name;
+
     @Size(max = 200)
     @NotNull
     @NotBlank
     private String description;
+
     private LocalDate releaseDate;
+
     @Positive
     @NotNull
     private Long duration;
